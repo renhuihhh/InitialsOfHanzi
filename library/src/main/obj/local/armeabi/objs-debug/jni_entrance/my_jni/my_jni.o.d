95e633462bf741f0ed29c17ed1fa2caaa65a4988
renhui <renh@haierubic.com>
2016-10-29 02:04:37 +0800

    delete binary files

--- library/src/main/obj/local/armeabi/objs-debug/jni_entrance/my_jni/my_jni.o.d
+++
@@ -1,7 +0,0 @@
-obj/local/armeabi/objs-debug/jni_entrance/./my_jni/my_jni.o: \
- jni/./my_jni/my_jni.cpp jni/./my_jni/../header/my_jni/my_jni.h \
- jni/iconv/srclib/string.h
-
-jni/./my_jni/../header/my_jni/my_jni.h:
-
-jni/iconv/srclib/string.h:
